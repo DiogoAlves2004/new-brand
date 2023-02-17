0a9fb2eb26f7d7574d09b4a5f473611c424ee3fc
Didizin <alvessilva524@gmail.com>
2023-02-17 09:53:13 -0300

    stable0.3

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -24,18 +24,18 @@ function Portfolio(){
             
                 <div>
                     <ProjectCard
-                        image={'https://i2.wp.com/gatinhobranco.com/wp-content/uploads/2020/04/vitrine-do-bem-gatos-Photo-by-Stratman.jpg?fit=800%2C515'}
+                        image={'https://blog.even3.com.br/wp-content/uploads/2021/12/imagem_destaque_projetodepesquisa.png'}
                         text={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing'}
-                        title={'Projeto1'}
+                        title={'Projeto 1'}
                     />
 
                 </div>
 
                 <div>
                     <ProjectCard
-                        image={'https://i2.wp.com/gatinhobranco.com/wp-content/uploads/2020/04/vitrine-do-bem-gatos-Photo-by-Stratman.jpg?fit=800%2C515'}
+                        image={'https://blog.even3.com.br/wp-content/uploads/2021/12/imagem_destaque_projetodepesquisa.png'}
                         text={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing'}
-                        title={'Projeto1'}
+                        title={'Projeto 2'}
                     />
 
                 </div>
