62b874f0f4cb3029762b45d164d201b3a22e5786
Didizin <alvessilva524@gmail.com>
2023-02-17 09:50:24 -0300

    stable0.3

--- src/pages/squad/Squad.jsx
+++ src/pages/squad/Squad.jsx
@@ -12,12 +12,12 @@ function Squad(){
 
             <div className={styles.squad_box}>
 
-                <SquadCard image='http://www.meupositivo.com.br/doseujeito/wp-content/uploads/2022/08/Como-remover-sua-foto-de-perfil-das-redes-sociais.jpg' name='Teste da silva ' title='ze ruela'  />
+                <SquadCard image='http://www.meupositivo.com.br/doseujeito/wp-content/uploads/2022/08/Como-remover-sua-foto-de-perfil-das-redes-sociais.jpg' name='Teste 1' title='Frontend Developer'  />
 
-                <SquadCard image='http://www.meupositivo.com.br/doseujeito/wp-content/uploads/2022/08/Como-remover-sua-foto-de-perfil-das-redes-sociais.jpg' name='Teste da silva ' title='ze ruela'  />
+                <SquadCard image='http://www.meupositivo.com.br/doseujeito/wp-content/uploads/2022/08/Como-remover-sua-foto-de-perfil-das-redes-sociais.jpg' name='Teste 2' title='Backend Developer'  />
 
 
-                <SquadCard image='http://www.meupositivo.com.br/doseujeito/wp-content/uploads/2022/08/Como-remover-sua-foto-de-perfil-das-redes-sociais.jpg' name='Teste da silva ' title='ze ruela'  />
+                <SquadCard image='http://www.meupositivo.com.br/doseujeito/wp-content/uploads/2022/08/Como-remover-sua-foto-de-perfil-das-redes-sociais.jpg' name='Teste 3' title='UI/UX Designer'  />
             </div>
 
         </section>
